575affe716dae4904c3ac798d4606837a26cb503
Phil Webster <pwebster@athenahealth.com>
2016-03-23 23:59:21 -0700

    Initial Commit

---
+++ NOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NOS
+//
+//  Created by Philip Webster on 3/23/16.
+//  Copyright © 2016 phil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
