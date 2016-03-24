d52b40226adc68ef4480a57a67c55de4a1ffe0ee
Phil Webster <pwebster@athenahealth.com>
2016-03-24 00:35:10 -0700

    hard-coded volume control in today

---
+++ NOS/SonosController.swift
@@ -0,0 +1,13 @@
+//
+//  SonosController.swift
+//  NOS
+//
+//  Created by Philip Webster on 3/24/16.
+//  Copyright © 2016 phil. All rights reserved.
+//
+
+import UIKit
+
+class SonosController: NSObject {
+
+}
